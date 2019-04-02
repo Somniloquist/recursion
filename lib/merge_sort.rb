f205b647842dde4c72640b313c9f49b4254fff9f
Robin Unger <robin.e.unger@gmail.com>
2019-04-02 17:42:58 -0500

    Complete merge_sort function

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -1,2 +1,18 @@
 module MergeSort
+  def merge_sort(arr, result = [])
+    return arr if arr.length < 2
+    left  = merge_sort(arr[0...arr.length/2])
+    right = merge_sort(arr[arr.length/2..-1])
+    
+    until left.empty? && right.empty? do
+      if left[0].nil?
+        result << right.shift
+      elsif right[0].nil?
+        result << left.shift
+      else
+        left[0] < right[0] ? result << left.shift : result << right.shift
+      end
+    end
+    result
+  end
 end
--- recursion.rb
+++ recursion.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require "./lib/fibonacci.rb"
 require "./lib/merge_sort.rb"
-
+include MergeSort
 
 puts("fibs".ljust(75, "="))
 p Fibonacci.fibs(5)
@@ -11,4 +11,7 @@ puts("fibs_rec".ljust(75, "="))
 p Fibonacci.fibs_rec(5)
 p Fibonacci.fibs_rec(6)
 p Fibonacci.fibs_rec(10)
-
+puts("merge_sort".ljust(75, "="))
+p merge_sort([5,2,5,5,2,1,6,8,9,0,1])
+p merge_sort(Array.new(10){rand(100)})
+p merge_sort(Array.new(10){rand(100)})
